bd9b00ffa6c1180f369c312ab723a3594de752e7
k5123 <github.commit@k512.studio>
2025-04-05 22:07:14 -0400

    Added systemd unit with sample initialization

--- spoof-device.py
+++ spoof-device.py
@@ -4,6 +4,7 @@ import evdev
 import sys
 from evdev import ecodes, AbsInfo
 from evdev import ecodes, AbsInfo, AbsEvent
+from evdev.ecodes import BTN_MISC
 
 devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
 
@@ -12,13 +13,15 @@ if not devices:
           'Check your evdev installation and device permissions.')
     sys.exit(1)
 
-if len(sys.argv) == 1:
+namemap = {}
+for d in devices:
+    namemap[d.name] = d.path
+
+if len(sys.argv) != 2 or sys.argv[1] not in namemap.keys():
     print("Please specify device to spoof.")
     print("Available devices:")
-    namemap = {}
-    for d in devices:
-        namemap[d.name] = d.path
-        print(d.path, d.name)
+    for d in namemap.keys():
+        print(d)
     sys.exit(1)
 
 device = None
@@ -38,11 +41,40 @@ caps = { #this won't work: device.capabilities()
 }
 
 caps[ecodes.EV_KEY] = [ #caps for key codes we send. refer to map below!
-    ecodes.BTN_JOYSTICK, ecodes.BTN_TRIGGER,
+    ecodes.BTN_JOYSTICK, ecodes.BTN_TRIGGER, BTN_MISC,
     ecodes.BTN_0, ecodes.BTN_1, ecodes.BTN_2, ecodes.BTN_3,
     ecodes.BTN_4, ecodes.BTN_5, ecodes.BTN_6, ecodes.BTN_7, ecodes.BTN_8, ecodes.BTN_9,
-
+    ecodes.BTN_X, ecodes.BTN_Y, #ecodes.BTN_A
 ]
+
+eventLUT = { #this maps incoming events to outgoing events by changing their code.
+    ecodes.REL_X: ecodes.ABS_X,
+    ecodes.REL_Y: ecodes.ABS_Y,
+    ecodes.REL_Z: ecodes.ABS_Z,
+    ecodes.REL_RX: ecodes.ABS_RX,
+    ecodes.REL_RY: ecodes.ABS_RY,
+    ecodes.REL_RZ: ecodes.ABS_RZ,
+
+    282: ecodes.BTN_JOYSTICK,
+
+    268: ecodes.BTN_0, #1
+    269: ecodes.BTN_1, #2
+    270: ecodes.BTN_2, #3
+    271: ecodes.BTN_3, #4
+
+    278: ecodes.BTN_4, #5
+    280: ecodes.BTN_5, #6
+    281: ecodes.BTN_6, #7
+    279: ecodes.BTN_7, #8
+
+    256: ecodes.BTN_A, #A?
+    257: ecodes.BTN_JOYSTICK, #B?
+
+    264: ecodes.BTN_8, #9
+    258: ecodes.BTN_Y, #?
+    261: ecodes.BTN_9, #10
+    260: ecodes.BTN_X #?
+}
 
 if not ecodes.EV_ABS in device.capabilities():
     # 3d space mouse reports only relative axes, so we need to set up absolute ones
@@ -58,7 +90,7 @@ if not ecodes.EV_ABS in device.capabilities():
 
 try:
     spoofdevice = evdev.uinput.UInput(events=caps,
-                                     name="Joystick 3dx" + " PRO",
+                                     name=d.name + " Joystick",
                                      vendor=0xdead, #device.info.vendor,
                                      product=0xbeef, #device.info.product,
                                      version=device.info.version)
@@ -68,39 +100,9 @@ except Exception as e:
 
 print("Spoofing:", spoofdevice)
 print("Mirroring events...")
-eventLUT = { #this maps incoming events to outgoing events by changing their code.
-    ecodes.REL_X: ecodes.ABS_X,
-    ecodes.REL_Y: ecodes.ABS_Y,
-    ecodes.REL_Z: ecodes.ABS_Z,
-    ecodes.REL_RX: ecodes.ABS_RX,
-    ecodes.REL_RY: ecodes.ABS_RY,
-    ecodes.REL_RZ: ecodes.ABS_RZ,
-    282: ecodes.BTN_JOYSTICK,
-
-    268: ecodes.BTN_0, #1
-    269: ecodes.BTN_1, #2
-    270: ecodes.BTN_2, #3
-    271: ecodes.BTN_3, #4
-
-    278: ecodes.BTN_4, #5
-    280: ecodes.BTN_5, #6?
-    281: ecodes.BTN_6, #7?
-    279: ecodes.BTN_7, #8
-
-    256: ecodes.BTN_A, #A?
-    257: ecodes.BTN_JOYSTICK, #B?
-
-    264: ecodes.BTN_X, #?
-    258: ecodes.BTN_Y, #?
-    261: ecodes.BTN_Z, #?
-    260: ecodes.BTN_BASE2 #?
-}
-
-def mirror():
-    pass
 
 for event in device.read_loop():
-    print(event)
+    #print(event)
     if event.type == ecodes.EV_REL:
         event.type = ecodes.EV_ABS
         event.code = eventLUT[event.code]
